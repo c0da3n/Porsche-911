e32706b35eac662920465d2c1e32eda04f250e89
c0da3n <sky@codawn.dev>
2025-05-26 01:08:57 -0400

    feat: Create sub-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Header from "./components/Header";
-import Background from "./assets/Background.png";
 import { useTranslation } from "react-i18next";
 import ScrollToTop from "./components/ScrollToTop";
 import CarInfo from "./components/CarInfo";
@@ -8,19 +7,29 @@ import MobileCarInfo from "./components/MobileCarInfo";
 
 const App = () => {
   const { t } = useTranslation();
+
+  const readScrollDown = () => {
+    window.scrollTo({
+      top:
+        document.querySelector(".Info")!.getBoundingClientRect().top +
+        window.scrollY,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="flex flex-col min-h-screen overflow-y-auto no-scrollbar">
       <Header />
-      <main
-        className="relative h-[calc(100vh-80px)] max-w-full bg-cover bg-center"
-        style={{ backgroundImage: `url(${Background})` }}
-      >
+      <main className="background">
         <div className="flex flex-col space-y-3 ml-10 max-md:ml-5 mt-40 max-md:mt-28 ">
           <h1 className="hero-title">{t("title")}</h1>
           <h3 className="subtitle">{t("subtitle")}</h3>
         </div>
 
-        <button className="absolute bottom-10 left-1/2 -translate-x-1/2 border-white border-2 px-10 py-2.5 flex items-center justify-center ">
+        <button
+          onClick={readScrollDown}
+          className="absolute bottom-10 left-1/2 -translate-x-1/2 border-white border-2 px-10 py-2.5 flex items-center justify-center "
+        >
           <p className="text-white font-medium  text-base">Read More</p>
         </button>
       </main>
@@ -28,6 +37,23 @@ const App = () => {
         <CarInfo />
         <MobileCarInfo />
         <CarImage />
+      </div>
+      <div className="Sub">
+        <div className="Sub-Title">
+          <h1>Overwhelm Many.</h1>
+          <h4>While making others celebrate.</h4>
+        </div>
+        <div className="Sub-Subtitle">
+          <p>
+            The 911 Turbo models feature incredible performance
+            <br />
+            combined with breath taking driving dynamics.
+          </p>
+          <p>
+            Even though the basic engine layout has not changed,
+            <br /> every detail has been optimized, honed and refined.
+          </p>
+        </div>
       </div>
       <ScrollToTop />
     </div>
